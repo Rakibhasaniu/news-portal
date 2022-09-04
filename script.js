a27105eb00e835adb6a2983af891c7eadac1fbe7
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-09-04 23:41:58 +0600

    loadCategory added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// const loadCategory = () => {
-//   fetch(`https://openapi.programming-hero.com/api/news/categories`)
-//     .then((res) => res.json())
-//     .then((data) => displayCategory(data.data.news_category))
-//     .catch((error) => displayCategory(error));
-// };
+const loadCategory = () => {
+  fetch(`https://openapi.programming-hero.com/api/news/categories`)
+    .then((res) => res.json())
+    .then((data) => displayCategory(data.data.news_category))
+    .catch((error) => displayCategory(error));
+};
 
 // const displayCategory = (catid) => {
 
