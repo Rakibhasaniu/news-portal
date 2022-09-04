df57d7f87e5d95331b0a0693f25bb43b23107674
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-09-04 23:47:47 +0600

    last section

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const toggleLoader = (isLoading) => {
 };
 
 
-// // displayModalDetailes();
+// displayModalDetailes();
 
-// loadCategoryDetails();
-// loadCategory();
+loadCategoryDetails();
+loadCategory();
